9492ddc1ef477aa0a8e19d9d02b58046f9779226
김영훈 <joseph0926@naver.com>
2024-03-03 16:57:26 +0900

    #12 Backend: Auth - 기본 구성

---
+++ server/packages/auth-service/src/app.ts
@@ -0,0 +1,9 @@
+import express, { Express } from 'express';
+import { start } from '@auth/server';
+
+const initialize = (): void => {
+  const app: Express = express();
+  start(app);
+};
+
+initialize();
---
+++ server/packages/auth-service/src/server.ts
@@ -0,0 +1,75 @@
+import http from 'http';
+
+import 'express-async-errors';
+import { CustomError, IErrorResponse } from '@base/custom-error-handler';
+import { winstonLogger } from '@base/logger';
+import { Logger } from 'winston';
+import { config } from '@auth/config';
+import { Application, Request, Response, NextFunction, json, urlencoded } from 'express';
+import hpp from 'hpp';
+import helmet from 'helmet';
+import cors from 'cors';
+import compression from 'compression';
+import { checkConnection } from '@auth/elasticsearch';
+import { appRoutes } from '@auth/routes';
+
+const SERVER_PORT = 4002;
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'authElasticSearchServer', 'debug');
+
+export function start(app: Application): void {
+  securityMiddleware(app);
+  standardMiddleware(app);
+  routesMiddleware(app);
+  startElasticSearch();
+  authErrorHandler(app);
+  startServer(app);
+}
+
+function securityMiddleware(app: Application): void {
+  app.set('trust proxy', 1);
+  app.use(hpp());
+  app.use(helmet());
+  app.use(
+    cors({
+      origin: config.API_GATEWAY_URL,
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+    })
+  );
+}
+
+function standardMiddleware(app: Application): void {
+  app.use(compression());
+  app.use(json({ limit: '200mb' }));
+  app.use(urlencoded({ extended: true, limit: '200mb' }));
+}
+
+function routesMiddleware(app: Application): void {
+  appRoutes(app);
+}
+
+function startElasticSearch(): void {
+  checkConnection();
+}
+
+function authErrorHandler(app: Application): void {
+  app.use((error: IErrorResponse, _req: Request, res: Response, next: NextFunction) => {
+    log.log('error', `AuthService ${error.comingFrom}:`, error);
+    if (error instanceof CustomError) {
+      res.status(error.statusCode).json(error.serializeErrors());
+    }
+    next();
+  });
+}
+
+function startServer(app: Application): void {
+  try {
+    const httpServer: http.Server = new http.Server(app);
+    log.info(`Auth server has started with process id ${process.pid}`);
+    httpServer.listen(SERVER_PORT, () => {
+      log.info(`Auth server running on port ${SERVER_PORT}`);
+    });
+  } catch (error) {
+    log.log('error', 'AuthService startServer() method error:', error);
+  }
+}

df4c1de53659fb0632ebc096177a2a082dfacc2a
김영훈 <joseph0926@naver.com>
2024-03-06 12:51:45 +0900

    #16 Frontend: 프로젝트 기본 구성

---
+++ client/libs/auth-lib/src/lib/auth-lib.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface AuthLibProps {}
+
+export function AuthLib(props: AuthLibProps) {
+  return (
+    <div>
+      <h1>Welcome to AuthLib!</h1>
+    </div>
+  );
+}
+
+export default AuthLib;
